8c21de10499fddc0c6e8571821d8e9fcaa7e709a
Idan- <Idan-@users.noreply.github.com>
2019-02-01 21:37:09 +0200

    chmod +x run script

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cp ./magicLockLayout /usr/bin/
+sudo chmod +x /usr/bin/magicLockLayout
 echo "# magically change keyboard layout to english on lock screen" >> ~/.profile
 echo "magicLockLayout &" >> ~/.profile
